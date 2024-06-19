276c89f4b25313a19bcfe89ccb1b6c16d93a0c5d
stackcats <stackcats.hk@gmail.com>
2024-06-19 15:55:45 +0800

    add git-conflict.nvim

--- lua/configs/plugins/init.lua
+++ lua/configs/plugins/init.lua
@@ -122,6 +122,11 @@ local plugins = {
         end,
     },
     {
+        "akinsho/git-conflict.nvim",
+        version = "*",
+        config = true,
+    },
+    {
         "lewis6991/gitsigns.nvim",
         config = function()
             require("configs.editor.gitsigns")
