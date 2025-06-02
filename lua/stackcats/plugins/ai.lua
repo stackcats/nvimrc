7747d5e1d9433beeef331b18582101374d3ef0f9
stackcats <stackcats.hk@gmail.com>
2025-06-02 13:24:19 +0800

    feat: update to windsurf

--- lua/stackcats/plugins/ai.lua
+++ lua/stackcats/plugins/ai.lua
@@ -1,5 +1,9 @@
 return {
-  "Exafunction/codeium.nvim",
+  "Exafunction/windsurf.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
   config = function()
     require("codeium").setup({})
   end,
