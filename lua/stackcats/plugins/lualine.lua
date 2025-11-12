70251e40d9b3cab99c954ffa16ad01191d0012b3
stackcats <stackcats.hk@gmail.com>
2025-11-12 13:25:04 +0800

    fix: update to vim.lsp.get_clients

--- lua/stackcats/plugins/lualine.lua
+++ lua/stackcats/plugins/lualine.lua
@@ -125,7 +125,7 @@ local dia = {
 
 local get_lsp_name = function()
   local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-  local clients = vim.lsp.get_active_clients()
+  local clients = vim.lsp.get_clients()
   if next(clients) == nil then
     return "No Active Lsp"
   end
